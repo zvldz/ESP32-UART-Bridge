a601b356c80730ce7cd830588bc0977e5a914f78
zvldz <zvldz@users.noreply.github.com>
2025-08-03 03:43:36 +0300

    remove USB Auto mode and reorganize TODO structure

--- src/defines.h
+++ src/defines.h
@@ -33,7 +33,7 @@ these features but is not justified for current requirements.
 
 // Device identification
 #define DEVICE_NAME "ESP32 UART Bridge"
-#define DEVICE_VERSION "2.10.0"
+#define DEVICE_VERSION "2.10.1"
 
 // Hardware pins - Device 1 (Main UART)
 #define BOOT_BUTTON_PIN 0
